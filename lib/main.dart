989fc753b6d5cfeec59b7667de89b86e6303014a
Ryouhei Furugen <furugenict8@gmail.com>
2021-12-23 13:59:04 +0900

    #13  YoutubePlayerBuilderとYoutubePlayerControllerを作ってみた。

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,75 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('youtube player demo'),
         ),
-        body: Container(
-          child: Column(),
+        body:YoutubePlayerBuilder(
+          player: YoutubePlayer(
+            controller: _controller,
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.blueAccent,
+
+            // Adds custom top bar widgets.
+            topActions: <Widget>[
+              const SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  _controller.metadata.title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                  size: 25.0,
+                ),
+                onPressed: () {
+                  // TODO 何か;
+                },
+              ),
+            ],
+            onReady: () {
+              // TODO　何か
+            },
+          ),
+          builder: (context, player) => Scaffold(
+            appBar: AppBar(
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Image.asset(
+                  'assets/ypf.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              title: const Text(
+                'YoutubePlayerFlutterDemo',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            body: const Text(
+              'ListViewがあったところ'
+            ),
+          ),
         ),
         // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
 }
+
+var _controller = _controller = YoutubePlayerController(
+  initialVideoId: 'nPt8bK2gbaU',
+  flags: const YoutubePlayerFlags(
+    mute: false,
+    autoPlay: true,
+    disableDragSeek: false,
+    loop: false,
+    isLive: false,
+    forceHD: false,
+    enableCaption: true,
+  ),
+);
