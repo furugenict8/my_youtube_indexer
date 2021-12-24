1251c4120c7a65ada7c87f343e1f1497a23d5237
Ryouhei Furugen <furugenict8@gmail.com>
2021-12-24 11:12:02 +0900

    #15 fix: 不要と思われるMyAppのbodyのScafoldを削除しexception "Unable to load asset: assets/ypf.png"が出ないようにした。YoutubePlayerBuilderをbuildする　StatelessWidgetを作って、bodyに入れた。

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(key: null,));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({required Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,67 +16,62 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('youtube player demo'),
         ),
-        body:YoutubePlayerBuilder(
-          player: YoutubePlayer(
-            controller: _controller,
-            showVideoProgressIndicator: true,
-            progressIndicatorColor: Colors.blueAccent,
-
-            // Adds custom top bar widgets.
-            topActions: <Widget>[
-              const SizedBox(width: 8),
-              Expanded(
-                child: Text(
-                  _controller.metadata.title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-              ),
-              IconButton(
-                icon: const Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                  size: 25.0,
-                ),
-                onPressed: () {
-                  // TODO 何か;
-                },
-              ),
-            ],
-            onReady: () {
-              // TODO　何か
-            },
-          ),
-          builder: (context, player) => Scaffold(
-            appBar: AppBar(
-              leading: Padding(
-                padding: const EdgeInsets.only(left: 12.0),
-                child: Image.asset(
-                  'assets/ypf.png',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              title: const Text(
-                'YoutubePlayerFlutterDemo',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            body: const Text(
-              'ListViewがあったところ'
-            ),
-          ),
-        ),
+        body: const YoutubePlayerFlutterExample(),
         // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
 }
 
-var _controller = _controller = YoutubePlayerController(
+class YoutubePlayerFlutterExample extends StatelessWidget {
+  const YoutubePlayerFlutterExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+        progressIndicatorColor: Colors.blueAccent,
+
+        // Adds custom top bar widgets.
+        topActions: <Widget>[
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              _controller.metadata.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+              size: 25,
+            ),
+            onPressed: () {
+              // TODO 何か;
+            },
+          ),
+        ],
+        onReady: () {
+          // TODO　何か
+        },
+      ),
+      builder: (context, player) => const Text(
+          'ListViewがあったところ'
+      ),
+    );
+  }
+}
+
+
+var _controller = YoutubePlayerController(
   initialVideoId: 'nPt8bK2gbaU',
   flags: const YoutubePlayerFlags(
     mute: false,
