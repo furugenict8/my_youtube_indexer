b73489e77de62d2f6d6a6f1166c66cef264b38c7
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-06 19:47:53 +0900

    #24 List<String> itemsをYoutubePlayerFlutterExampleの中に移動、その他もろもろ変更

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
   runApp(const MyApp(key: null,));
 }
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   const MyApp({required Key? key}) : super(key: key);
@@ -19,7 +18,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('youtube player demo'),
         ),
-        body: const YoutubePlayerFlutterExample(),
+        body: YoutubePlayerFlutterExample(
+          items: List<String>.generate(10000, (i) => 'Item $i'),
+        ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
               //TODO(自分): 押したら動画の現在時刻を取得して表示する,
@@ -33,10 +34,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final items = List<String>.generate(10000, (i) => 'Item $i');
 
 class YoutubePlayerFlutterExample extends StatelessWidget {
-  const YoutubePlayerFlutterExample({Key? key}) : super(key: key);
+  const YoutubePlayerFlutterExample(
+      {Key? key,
+        required this.items
+      }
+      ) : super(key: key);
+  final List<String> items;
 
   @override
   Widget build(BuildContext context) {
